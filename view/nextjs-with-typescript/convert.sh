8998d8e670897ea0ef4f4bc3b9861b7d5bd9050f
r-40021 <75155258+r-40021@users.noreply.github.com>
2022-03-15 00:23:58 +0900

    [Update]Shell

--- view/nextjs-with-typescript/convert.sh
+++ view/nextjs-with-typescript/convert.sh
@@ -1,26 +1,23 @@
 #!/bin/sh
 jsDirs=`find ./out/_next -type f -and -name \*.js`
+htmlDirs=`find ./out -type f -and -name \*.html`
 mkdir -p gas
 
-for dir in $jsDirs;
+for jsDir in $jsDirs;
 do
-  echo $dir
-  fileName=`basename $dir`
+  fileName=`basename $jsDir`
   newPath=./gas/${fileName}.html
-  scp $dir $newPath
+  scp $jsDir $newPath
   sed -i -e "1s/^/<script>/" ${newPath}
   sed -i "\$a </script>" ${newPath}
-done
+  echo ${jsDir}
 
-
-# ここからHTMLの処理
-htmlDirs=`find ./out -type f -and -name \*.html`
-
-for dir in $htmlDirs;
-do
-  echo $dir
-  fileName=`basename $dir`
-  newPath=./gas/${fileName}
-  scp $dir $newPath
-  sed -i "\s/src=\"/_next/static//src=\".//g" ${newPath}
+  for htmlDir in $htmlDirs;
+  do
+    fileName=`basename $htmlDir`
+    newPath=./gas/${fileName}
+    scp $htmlDir $newPath
+    relativeJSPath=`sed -e 's/./' ${jsDir}`
+    sed -i -e 's/\<script src="${relativeJSPath}" defer=""\>\<\/script\>/いれかえ/' ${newPath}
+  done
 done
